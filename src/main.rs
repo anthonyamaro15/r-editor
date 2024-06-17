c8da26249069f71e2d12ad77ede5d32eeb9c4249
Anthony Amaro <anthonyamaro5555@gmail.com>
2024-06-16 19:57:15 -0700

    feat: add basic navigation commands for normal mode

--- src/main.rs
+++ src/main.rs
@@ -6,51 +6,113 @@
 
 mod buffer;
 
-use std::io::{self, Write};
+use std::io::{self, stdout, Write};
 
 use buffer::Buffer;
 use crossterm::{
+    cursor,
     event::{self, KeyCode},
     execute,
-    terminal::{self, EnterAlternateScreen, LeaveAlternateScreen},
+    style::{self, Stylize},
+    terminal::{
+        self, disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen,
+    },
+    QueueableCommand,
 };
+
+#[derive(Debug, Clone, Copy)]
+enum Mode {
+    Normal,
+    Insert,
+}
+
+// rows => top to bottom
+// columns => left to right
+//
+// y => top to bottom
+// x => left to right
+
+struct Editor {
+    mode: Mode,
+    column_position: usize,
+    row_position: usize,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            mode: Mode::Normal,
+            column_position: 0,
+            row_position: 0,
+        }
+    }
+    fn draw_editor(&mut self, stdout: &mut io::Stdout) -> anyhow::Result<()> {
+        stdout.flush()?;
+        stdout.queue(cursor::MoveTo(
+            self.column_position as u16,
+            self.row_position as u16,
+        ))?;
+        stdout.flush()?;
+
+        Ok(())
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     execute!(io::stdout(), EnterAlternateScreen)?;
+    enable_raw_mode()?;
+    //let mut buffer = Buffer::new();
+    let mut editor = Editor::new();
 
-    let mut buffer = Buffer::new();
+    let mut stdout = stdout();
 
+    execute!(stdout, terminal::Clear(terminal::ClearType::All))?;
     loop {
-        print_buffer(&buffer)?;
+        //print_buffer(&buffer)?;
 
-        if let event::Event::Key(event) = event::read()? {
-            match event.code {
-                KeyCode::Char(c) => {
-                    buffer.insert(c);
-                }
-                KeyCode::Backspace => {
-                    //buffer.delete();
-                }
-                KeyCode::Left => {
-                    buffer.move_cursor_left();
-                }
-                KeyCode::Right => {
-                    buffer.move_cursor_right();
-                }
-                KeyCode::Enter => {
-                    buffer.insert('\n');
-                }
-                KeyCode::Esc => break,
+        editor.draw_editor(&mut stdout)?;
+
+        match editor.mode {
+            Mode::Normal => match event::read()? {
+                event::Event::Key(event) => match event.code {
+                    KeyCode::Char('q') => break,
+                    KeyCode::Char('i') => {
+                        editor.mode = Mode::Insert;
+                    }
+                    KeyCode::Char('h') => {
+                        if editor.column_position > 0 {
+                            editor.column_position -= 1;
+                        }
+                    }
+                    KeyCode::Char('l') => {
+                        editor.column_position += 1;
+                    }
+
+                    KeyCode::Char('k') => {
+                        if editor.row_position > 0 {
+                            editor.row_position -= 1;
+                        }
+                    }
+                    KeyCode::Char('j') => {
+                        editor.row_position += 1;
+                    }
+                    _ => {}
+                },
                 _ => {}
+            },
+            Mode::Insert => {
+                todo!()
             }
         }
     }
 
+    disable_raw_mode()?;
     execute!(io::stdout(), LeaveAlternateScreen)?;
+    stdout.flush()?;
     Ok(())
 }
 
-fn print_buffer(buffer: &Buffer) -> anyhow::Result<()> {
+/* fn print_buffer(buffer: &Buffer) -> anyhow::Result<()> {
     let cursor_position = buffer.get_cursor_position();
     let content = buffer.get_content();
 
@@ -84,4 +146,4 @@ fn calculate_position(cursor_position: usize, content: &str) -> (u16, u16) {
         }
     }
     (col, row)
-}
+} */
